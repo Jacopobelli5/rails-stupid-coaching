713eda51db62daee99180b284666323c8a986a4f
jacopo belli <jacopo.b2016@gmail.com>
2022-07-12 20:38:09 +0100

    app completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    @answer = if @question ==  'I am going to work'
+                'Great!'
+              elsif @question == /[^.!?]+\?/
+                'Silly question, get dressed and go to work!'
+              else
+                'I don`t care, get dressed and go to work!'
+              end
   end
 end
